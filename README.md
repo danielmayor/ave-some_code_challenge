b7e12e0c0d7972f5c82dca7032cae9a74a2c34ed
Daniel Mayor <cacique500@gmail.com>
2020-05-21 22:01:45 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# awesome_code_challenge
+# Ave-some code challenge!
+Hi! Welcome to the obfuscator.
+
+# Files
+ - obfuscator.py
+
+## Environment tested
+Windows 10 + Python version 3.8.3
+
+## Syntax
+[python3] obfuscator.py <input_file>
+
+e.g.: *& python.exe obfuscator.py AvevaCodeExcercise.input.json*
+
+e.g.: *.\obfuscator.py .\test\AvevaCodeExcercise.input.json*
+
+e.g.: *& python.exe .\obfuscator.py c:\test\AvevaCodeExcercise.input.json*
+
+# Input file encoding
+As provided by the entity (UTF-16/UCS-2).
+
+# Test provided file
+ - AvevaCodeExcercise.input.json
+
---
+++ obfuscator.py
@@ -0,0 +1,63 @@
+﻿from pathlib import Path
+import sys, getopt
+
+def get_file_content(input_file):
+    with open(input_file, 'rb') as f:
+      content = f.read()
+      f.close()
+      content = content.decode('utf-16')
+      content = content.split('\r\n')
+    return content
+
+def process_file(file_content):
+    truncated_file_content = []
+    for line in file_content:
+        if line.strip() != '':
+            lista = line.split(' : ')
+            if lista[0].endswith('-Password') or lista[0].endswith('-Key') or lista[0].endswith('-ClientSecret'):
+                lista[1] = truncate(lista[1])
+            elif 'password=' in lista[1]:
+                lista[1] = replace_value(lista[1],'password=',',')
+            elif 'AccountKey=' in lista[1]:
+                lista[1] = replace_value(lista[1],'AccountKey=',';')
+            truncated_lista = ' : '.join(lista)
+            truncated_file_content.append(truncated_lista)
+    return truncated_file_content
+
+def truncate(value):
+    truncated = value[:5] + '...' + value[-5:]
+    return truncated
+
+def replace_value(value, key, separator):
+    stage1 = value.split(separator)
+    stage3 = []
+    for element in stage1:
+        if key in element:
+            stage2 = element.split(key)
+            stage2[1] = truncate(stage2[1])
+            element = key.join(stage2)
+        stage3.append(element)
+    replaced = separator.join(stage3)
+    return replaced
+
+def generate_output_file(processed, name):
+    content_to_write = ''
+    for line in processed:
+        content_to_write += line + '\n'
+    f = open('obfuscated_' + name, 'w', encoding='utf-16')
+    f.write(content_to_write)
+    f.close()
+
+def main(arg):
+    if arg:
+        file_path = Path(arg[0])
+        file_content = get_file_content(file_path)
+        processed_content = process_file(file_content)
+        generate_output_file(processed_content, file_path.name)
+        print('File generated: ' + 'obfuscated_' + file_path.name)
+    else:
+        print('Syntax:')
+        print('aveva.py <input_file>')
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
